dcd9f8386eadb725596fde38ab2f06710b69b476
LochanRn <rajanaga_ra@srmuniv.edu.in>
2022-05-27 00:13:17 +0530

    updated docker-terraform with pre-post-hook changes

--- exec.sh
+++ exec.sh
@@ -1,5 +1,11 @@
 INPUT=$1
 
+echo "PREEXECCMD" ${PREEXECCMD}
+if ! ${PREEXECCMD} 
+then
+    exit 1
+fi
+
 if terraform init --plugin-dir /providers/plugins > /dev/null 2>&1; then
   terraform init --plugin-dir /providers/plugins # first check if provider is cached locally and if it is not present then download
 else
@@ -11,4 +17,10 @@ if [[ ${INPUT} == 'apply' ]]; then
   terraform apply -lock=false -auto-approve
 else
   terraform destroy -lock=false -auto-approve
+fi
+
+echo "POSTEXECCMD" ${POSTEXECCMD} 
+if ! ${POSTEXECCMD} 
+then
+    exit 1
 fi
